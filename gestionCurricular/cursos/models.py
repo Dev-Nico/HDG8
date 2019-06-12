d784834c363c4497d82bbc128726015753d26cb4
patata <darkflein@gmail.com>
2019-06-12 19:20:25 -0400

    update gestion

--- gestionCurricular/cursos/models.py
+++ gestionCurricular/cursos/models.py
@@ -2,17 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Curso:
-	def __init__(self,sigla,nombre,creditos):
-		self.sigla=sigla
-		self.nombre=nombre
-		self.creditos=creditos
+class Curso(models.Model):
+		sigla=models.CharField(max_length=6)
+		nombre=models.CharField(max_length=60)
+		creditos=models.IntegerField()
+		nrc=models.IntegerField(null=True)
+		cursos=models.Manager()
+
+		def __str__(self):
+			return "{}".format(self.nombre)
 
 class CursoFactory:
 	def __init__(self):
 		self.cursos=[]
 		self.cursos.append(Curso("ICF232","Ingeniería de Software I",6))
 		self.cursos.append(Curso("ICF121","Introducción a la Ingeniería Civil Informática",6))
+		#self.cursos.append(Curso("MAT021","Calculo Diferencial",6,2656))
+		#self.cursos.append(Curso("MAT011","Introducción a las Matemáticas",6,2643))
+		#self.cursos.append(Curso("MAT021","Calculo Diferencial",6,2554))
 
 	def obtenerCursos(self):
 		return self.cursos
--- gestionCurricular/cursos/views.py
+++ gestionCurricular/cursos/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Curso,CursoFactory
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+class CursoCreate(CreateView):
+	model = Curso
+	template_name='./curso_form.html'
+	fields = '__all__'
+
+class CursoUpdate(UpdateView):
+	model = Curso
+	template_name='./curso_form.html'
+	fields=['sigla','nombre','creditos','NRC']
+
+class CursoDelete(DeleteView):
+	model=Curso
+	template_name='./curso_confirm_delete.html'
+	success_url=reverse_lazy('cursos')
 
 class PermissionRequiredInGroupMixin(PermissionRequiredMixin):
 	def has_permission(self):
@@ -17,18 +34,26 @@ class PermissionRequiredInGroupMixin(PermissionRequiredMixin):
 		return True
 
 class HomePageView(TemplateView):
+
 	def get(self, request, **kwargs):
 		return render(request,'index.html', context=None)
 
-class HomeCursosView(PermissionRequiredInGroupMixin, LoginRequiredMixin, TemplateView):
+class HomeCursosView(PermissionRequiredMixin, LoginRequiredMixin, TemplateView):
+
 	permission_required='puede_buscar_cursos'
 	def get(self, request, **kwargs):
-		cursoFactory=CursoFactory()
-		return render(request,'cursos.html',{'cursos':cursoFactory.obtenerCursos()})
-		#return render(request, 'cursos.html',{'cursos':Curso.cursos.all()})
+		#return render(request,'cursos.html',{'cursos':cursoFactory.obtenerCursos()})
+		
+		return render(request, 'cursos.html',{'cursos':Curso.cursos.all()})
 
-class DetalleCursoView(LoginRequiredMixin, TemplateView):
+class HomeNRCView(PermissionRequiredMixin, LoginRequiredMixin, TemplateView):
+	permission_required='puede_buscar_cursos'
+	def get(self,request,**kwargs):
+		return render(request,'NRC.html',{'cursos':Curso.cursos.all()})
+
+class DetalleCursoView(PermissionRequiredMixin, LoginRequiredMixin, TemplateView):
+
+	permission_required='puede_buscar_cursos'
 	def get(self, request, **kwargs):
-		cursoFactory=CursoFactory()
 		sigla=kwargs["sigla"]
-		return render(request,'curso.html',{'curso':cursoFactory.getCurso(sigla)})
+		return render(request,'curso.html',{'curso':Curso.cursos.get(sigla=sigla)})
