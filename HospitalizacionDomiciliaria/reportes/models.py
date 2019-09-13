45b855649413214795ce4fd99b11371cbd9e6fda
Panchitoo <f.alvarezcorrea1@gmail.com>
2019-09-13 16:57:02 -0300

    Creacion de test para modelos disponibles

--- HospitalizacionDomiciliaria/reportes/models.py
+++ HospitalizacionDomiciliaria/reportes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class RegistroFuncionarioForm(models.Model):
+class ReporteFuncionario(models.Model):
     Reporte = models.CharField(max_length=1000)
 
 
