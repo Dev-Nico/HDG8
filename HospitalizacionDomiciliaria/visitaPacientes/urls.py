497d1035d969bfc4e13e1829043c7688688e8edb
joselmq <joselmq@hotmail.cl>
2019-11-25 14:45:17 -0300

    VisitaPacientes fixed

--- HospitalizacionDomiciliaria/visitaPacientes/urls.py
+++ HospitalizacionDomiciliaria/visitaPacientes/urls.py
@@ -2,10 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import VisitaPacientePageView
 
+
 urlpatterns = [
     path('', VisitaPacientePageView.as_view() ,name='visitaPacientes'),
 ]
-
-
-
-
